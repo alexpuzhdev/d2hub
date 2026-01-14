a0821ad3ebe4bcb1ea8f9c248efffbbbd1fa2cb0
alexpuzhdev <168548067+alexpuzhdev@users.noreply.github.com>
2026-01-14 23:12:34 +0300

    Add infrastructure ports

--- src/dota_hud/application/__init__.py
+++ src/dota_hud/application/__init__.py
@@ -1,5 +1,20 @@
 """Слой приложения HUD."""
 
 from .app_controller import AppController
+from .commands import HudAction
+from .models import GameStateSnapshot, HudState, WarningState
+from .ports import GsiServerPort, HotkeysPort, LogWatcherPort
+from .use_cases import HudCycleResult, HudCycleUseCase
 
-__all__ = ["AppController"]
+__all__ = [
+    "AppController",
+    "GameStateSnapshot",
+    "HudState",
+    "HudCycleResult",
+    "HudCycleUseCase",
+    "HudAction",
+    "GsiServerPort",
+    "HotkeysPort",
+    "LogWatcherPort",
+    "WarningState",
+]
